7b90d5875a0a7fea59ab38e703cd108ab5c95488
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2018-05-16 19:19:41 +0900

    Fix bugs & Add parameters

--- src/map_splitter.cpp
+++ src/map_splitter.cpp
@@ -1,9 +1,8 @@
 #include "map_splitter.h"
 
 MapSplitter::MapSplitter(){
-  entropy_sub = n.subscribe("slam_gmapping/entropy", 1, &MapSplitter::entropyCallback, this);
+  entropy_sub_ = nh_.subscribe("slam_gmapping/entropy", 1, &MapSplitter::entropyCallback, this);
   entropy_temp = 0;
-  entropy_th = -3.0;
   shell_script_generated = false;
 }
 
@@ -15,6 +14,10 @@ void MapSplitter::entropyCallback(const std_msgs::Float64::ConstPtr& msg){
 void MapSplitter::computeKidnappedTime(){
   double entropy_diff = entropy - entropy_temp; 
 
+  if(!private_nh_.getParam("entropy_threshold", entropy_th)){
+    entropy_th = -3.0;
+  }
+
   if(entropy_diff < entropy_th && !shell_script_generated){
     ros::Time t = ros::Time::now();
     generateShellScript(t.sec);
@@ -25,11 +28,18 @@ void MapSplitter::computeKidnappedTime(){
 }
 
 void MapSplitter::generateShellScript(double kidnapped_time){
+  if(!private_nh_.getParam("time_margin_1", time1)){
+    time1 = 4.0;
+  }
+  if(!private_nh_.getParam("time_margin_2", time2)){
+    time2 = 10.0;
+  }
+
   std::ofstream outputfile("split.sh");
 
   outputfile << "#! /bin/bash" << "\n";
-  outputfile << "rosbag filter target.bag target_before.bag \"t.secs <\"" << kidnapped_time << "\n";
-  outputfile << "rosbag filter target.bag target_after.bag \"t.secs >\"" << kidnapped_time << "\n";
+  outputfile << "rosbag filter target.bag target_before.bag \"t.secs < " << std::setprecision(10) << kidnapped_time - time1 << "\"" << "\n";
+  outputfile << "rosbag filter target.bag target_after.bag \"t.secs > " << std::setprecision(10) << kidnapped_time + time2 << "\"" << "\n";
 
   outputfile.close();
   ROS_INFO("Shell script generated.");
--- src/map_splitter.h
+++ src/map_splitter.h
@@ -10,8 +10,11 @@ public:
   double entropy_temp;
 
 private:
-  ros::NodeHandle n;
-  ros::Subscriber entropy_sub;
+  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_;
+  ros::Subscriber entropy_sub_;
+  double time1;
+  double time2;
   double entropy_th;
   bool shell_script_generated;
   void entropyCallback(const std_msgs::Float64::ConstPtr& msg);
